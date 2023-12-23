a3bd68e4385368e845a5336ebd752ecdc0d8f0c1
backsft <kabirmia526@gmail.com>
2023-12-24 00:14:14 +0600

    working code

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,39 @@
+let stompClient;
+
+function connect() {
+    const socket = new SockJS('/ws');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, onConnect, onError);
+}
+
+function onConnect() {
+    stompClient.subscribe('/topic/chat', onMessageReceived);
+}
+
+function onMessageReceived(payload) {
+    const message = JSON.parse(payload.body);
+    displayMessage(message);
+}
+
+function displayMessage(message) {
+    const chatMessages = document.getElementById('chat-messages');
+    const messageElement = document.createElement('div');
+    messageElement.textContent = message;
+    chatMessages.appendChild(messageElement);
+}
+
+function onError(error) {
+    console.error('WebSocket Error:', error);
+}
+
+function sendMessage() {
+    const messageInput = document.getElementById('message-input');
+    const message = messageInput.value.trim();
+
+    if (message !== '') {
+        stompClient.send('/app/send/message', {}, message);
+        messageInput.value = '';
+    }
+}
+
+connect();
